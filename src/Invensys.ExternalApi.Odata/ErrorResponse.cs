0804ae825cb54bcc50efd7797c48c38df0c1df5f
Ulbrich Frauendorf <ulbrich@invensys.web.za>
2025-04-02 12:39:17 +0200

    Odata Seperated

---
+++ src/Invensys.ExternalApi.Odata/ErrorResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Invensys.ExternalApi.Odata
+{
+    public class ErrorResponse
+    {
+        [JsonPropertyName("@odata.context")]
+        public string? OdataContext { get; set; }
+
+        [JsonPropertyName("Message")]
+        public string? Message { get; set; }
+
+        [JsonPropertyName("Details")]
+        public List<Detail> Details { get; } = [];
+
+        [JsonPropertyName("Success")]
+        public bool? Success { get; set; }
+    }
+
+    public class Detail
+    {
+        [JsonPropertyName("Message")]
+        public string? Message { get; set; }
+    }
+}
