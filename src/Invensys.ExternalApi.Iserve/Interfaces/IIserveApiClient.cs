59b59ce053d7ea5330f108366fe0df7712d02db8
Ulbrich Frauendorf <ulbrich@invensys.web.za>
2026-02-15 22:23:39 +0200

    Fix tests an remove unused implementation of get

--- src/Invensys.ExternalApi.Iserve/Interfaces/IIserveApiClient.cs
+++ src/Invensys.ExternalApi.Iserve/Interfaces/IIserveApiClient.cs
@@ -4,7 +4,5 @@ namespace Invensys.ExternalApi.Iserve.Interfaces;
 
 public interface IIserveApiClient
 {
-   Task<TRequest> GetAsync<TRequest>(JwtAccessTokenRequest accessTokenRequest, string url);
-   Task<List<TRequest>> GetListAsync<TRequest>(JwtAccessTokenRequest accessTokenRequest, string url);
    Task<TResponse> PostAsync<TResponse, TRequest>(JwtAccessTokenRequest accessTokenRequest, string url, TRequest requestBody);
 }
